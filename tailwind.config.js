a25917c160ae3b3c50775cb8df52cba2382591f4
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-12-14 20:50:54 +0200

    config files updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-	mode: "jit",
-	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	darkMode: "class", // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				default: ["'Signika', sans-serif"],
+				firaSans: ["'Fira Sans'"],
+			},
+		},
 	},
 	variants: {
 		extend: {},
